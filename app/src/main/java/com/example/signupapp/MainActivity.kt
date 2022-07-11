feaadc9dfb97557992dd1790c95f44a20cfc009e
parkjunsik <akfldkskgorn@gmail.com>
2022-07-12 01:07:51 +0900

    2022/07/12 Test Sign Up Application
    + use socket.io

--- app/src/main/java/com/example/signupapp/MainActivity.kt
+++ app/src/main/java/com/example/signupapp/MainActivity.kt
@@ -3,17 +3,23 @@ package com.example.signupapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.signupapp.databinding.ActivityMainBinding
+import io.socket.client.Socket
+import io.socket.emitter.Emitter
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    lateinit var socket :Socket
     lateinit var signUpResult: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        socket = SocketApplication.get()
+        socket.connect()
 
         signUpResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             if(it.resultCode == RESULT_OK) {
@@ -26,11 +32,33 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
         with(binding){
             signUpText.setOnClickListener{
                 val intent = Intent(this@MainActivity, SignUpActivity::class.java)
                 signUpResult.launch(intent)
             }
+            signInButton.setOnClickListener{
+                val jsonObject = JSONObject()
+                jsonObject.put("email",emailText.text.toString())
+                jsonObject.put("passWard",pwText.text.toString())
+                socket.emit("sign in request", jsonObject)
+                socket.on("sign in result", signInResult)
+            }
         }
+    }
+    val signInResult = Emitter.Listener { args ->
+        val obj = JSONObject(args[0].toString())
+        Thread(object : Runnable{
+            override fun run() {
+                runOnUiThread(Runnable {
+                    kotlin.run {
+                        if(obj.get("result") == "success"){
+                            finish()
+                        }
+                    }
+                })
+            }
+        }).start()
     }
 }
